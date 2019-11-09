15443bfad35b1329526ca9660b0d6937c3c804ab
Andrew Palmer <atp@sdf.org>
2019-11-09 17:09:54 -0600

    Break out test tree setup

--- main.c
+++ main.c
@@ -3,14 +3,19 @@
 #include "util.h"
 #include "tree.h"
 
-int main(void)
-{
+static struct tree_node *setup_tree(void) {
     struct tree_node *tree = tree_new();
     tree_add(&tree, 6);
     tree_add(&tree, 7);
     tree_add(&tree, 2);
     tree_add(&tree, 11);
     tree_add(&tree, 7);
+    return tree;
+}
+
+int main(void)
+{
+    struct tree_node *tree = setup_tree();
     printf("Node count is %d\n", tree_count(tree));
     printf("Tree height is %d\n", tree_height(tree));
     printf("Min value is %d\n", tree_min(tree));
