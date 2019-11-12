a19a50bbfc809183ff1b00db1e6c503806b0bd36
Andrew Palmer <atp@sdf.org>
2019-11-11 19:40:17 -0600

    Create test_tree_count test

--- main.c
+++ main.c
@@ -3,6 +3,13 @@
 #include "util.h"
 #include "tree.h"
 
+#define ASSERT(test) \
+    { \
+        int result = (int)(test); \
+        char display = result ? '.' : 'F'; \
+        printf("%c", display); \
+    }
+
 static struct tree_node *setup_tree(void) {
     struct tree_node *tree = tree_new();
     tree_add(&tree, 6);
@@ -13,10 +20,23 @@ static struct tree_node *setup_tree(void) {
     return tree;
 }
 
+static void test_tree_count(void) {
+    struct tree_node *tree = setup_tree();
+    int expected = 4;
+    int actual = tree_count(tree);
+    ASSERT(expected == actual);
+    tree_free(tree);
+}
+
+static void run_tests() {
+    test_tree_count();
+    printf("\n");
+}
+
 int main(void)
 {
+    run_tests();
     struct tree_node *tree = setup_tree();
-    printf("Node count is %d\n", tree_count(tree));
     printf("Tree height is %d\n", tree_height(tree));
     printf("Min value is %d\n", tree_min(tree));
     printf("Max value is %d\n", tree_max(tree));
